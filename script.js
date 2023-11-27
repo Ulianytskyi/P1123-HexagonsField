32643fcd283ea376a641bd2dcbe12ca53a9888b2
Artem Ulianytskyi <32933673+Ulianytskyi@users.noreply.github.com>
2023-11-27 17:24:56 +0200

    commit 1 - create project files

---
+++ script.js
@@ -0,0 +1,32 @@
+const container = document.getElementById("container");
+
+const hex1 = document.createElement("div");
+hex1.className = "hex hex00";
+const hex2 = document.createElement("div");
+hex2.className = "hex hex01";
+const hex3 = document.createElement("div");
+hex3.className = "hex hex02";
+
+const hex4 = document.createElement("div");
+hex4.className = "hex hex10";
+const hex5 = document.createElement("div");
+hex5.className = "hex hex11";
+const hex6 = document.createElement("div");
+hex6.className = "hex hex12";
+
+const hex7 = document.createElement("div");
+hex7.className = "hex hex20";
+const hex8 = document.createElement("div");
+hex8.className = "hex hex21";
+const hex9 = document.createElement("div");
+hex9.className = "hex hex22";
+
+container.appendChild(hex1);
+container.appendChild(hex2);
+container.appendChild(hex3);
+container.appendChild(hex4);
+container.appendChild(hex5);
+container.appendChild(hex6);
+container.appendChild(hex7);
+container.appendChild(hex8);
+container.appendChild(hex9);
