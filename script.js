942a38b144d89bebbed408ab9690b4af7b25e679
Artem Ulianytskyi <32933673+Ulianytskyi@users.noreply.github.com>
2023-11-28 09:44:52 +0200

    commit 3 - hex field generation

--- script.js
+++ script.js
@@ -1,32 +1,41 @@
 const container = document.getElementById("container");
 
-const hex1 = document.createElement("div");
-hex1.className = "hex hex00";
-const hex2 = document.createElement("div");
-hex2.className = "hex hex01";
-const hex3 = document.createElement("div");
-hex3.className = "hex hex02";
+const hexField = document.createElement("div");
+hexField.className = "hex-field";
+container.appendChild(hexField);
 
-const hex4 = document.createElement("div");
-hex4.className = "hex hex10";
-const hex5 = document.createElement("div");
-hex5.className = "hex hex11";
-const hex6 = document.createElement("div");
-hex6.className = "hex hex12";
+const inputField = document.createElement("div");
+inputField.className = "input-field";
+let inputFieldText = `
+        <label for="num-row">Enter row number</label>
+        <input type="number" id="num-row"><br><br>
+        <label for="num-col">Enter column number</label>
+        <input type="number" id="num-col"><br><br>
+        <button onclick="generateHexagonField()">Generate</button>
+    `;
+inputField.innerHTML = inputFieldText;
+container.appendChild(inputField);
 
-const hex7 = document.createElement("div");
-hex7.className = "hex hex20";
-const hex8 = document.createElement("div");
-hex8.className = "hex hex21";
-const hex9 = document.createElement("div");
-hex9.className = "hex hex22";
+function generateHexagonField() {
+  const rowsNumber = Number(document.getElementById("num-row").value);
+  const columnsNumber = Number(document.getElementById("num-col").value);
 
-container.appendChild(hex1);
-container.appendChild(hex2);
-container.appendChild(hex3);
-container.appendChild(hex4);
-container.appendChild(hex5);
-container.appendChild(hex6);
-container.appendChild(hex7);
-container.appendChild(hex8);
-container.appendChild(hex9);
+  if (rowsNumber && columnsNumber && rowsNumber > 0 && columnsNumber > 0) {
+    for (let j = 0; j < columnsNumber; j++) {
+      for (let i = 0; i < rowsNumber; i++) {
+        let shiftLeft = j % 2 == 0 ? 0 : 42;
+        let row = 85 * i + shiftLeft;
+        let col = j * 71;
+        console.log(col, row);
+        const hex = document.createElement("div");
+        hex.className = "hex";
+        hex.style.top = col + "px";
+        hex.style.left = row + "px";
+        hexField.appendChild(hex);
+      }
+    }
+    inputField.classList.add("hide");
+  } else {
+    inputField.innerHTML = inputFieldText + `<br><br>Enter numbers!`;
+  }
+}
